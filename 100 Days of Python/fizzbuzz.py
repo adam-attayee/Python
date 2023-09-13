15f49dac7dd33b4aa0f77580006ee16d440f2cd5
obaid-attayee <81998505+obaid-attayee@users.noreply.github.com>
2023-09-12 20:01:08 -0400

    Create fizzbuzz.py

---
+++ 100 Days of Python/fizzbuzz.py
@@ -0,0 +1,16 @@
+# Given an integer n, return:
+# answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+# answer[i] == "Fizz" if i is divisible by 3.
+# answer[i] == "Buzz" if i is divisible by 5.
+# answer[i] == i (as a string) if none of the above conditions are true.
+
+
+for i in range(1,101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
