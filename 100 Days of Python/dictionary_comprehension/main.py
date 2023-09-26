53c82ddab126d3cd99adf541c78ff42dc7306273
obaid-attayee <81998505+obaid-attayee@users.noreply.github.com>
2023-09-26 15:12:05 -0400

    Create main.py

---
+++ 100 Days of Python/dictionary_comprehension/main.py
@@ -0,0 +1,12 @@
+#Take a word as an input and for each letter in the word, output the nato phonetic alphabet word
+
+import pandas
+
+df = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+nato_alphabet = {value.letter: value.code for (key, value) in df.iterrows()}
+
+word = input("Enter a word: ").upper()
+
+code_word = [nato_alphabet[letter] for letter in word]
+print(code_word)
