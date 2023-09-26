27567ed41958f4d16f4c0970fc5e83298b0c09d4
obaid-attayee <81998505+obaid-attayee@users.noreply.github.com>
2023-09-26 15:13:36 -0400

    Update main.py

--- 100 Days of Python/dictionary_comprehension/main.py
+++ 100 Days of Python/dictionary_comprehension/main.py
@@ -1,4 +1,5 @@
 #Take a word as an input and for each letter in the word, output the nato phonetic alphabet word
+#For example: if the user enters "amazon", the output should be ['Alfa', 'Mike', 'Alfa', 'Zulu', 'Oscar', 'November']
 
 import pandas
 
